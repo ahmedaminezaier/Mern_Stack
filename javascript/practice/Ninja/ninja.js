8b3c7fd3cbaf75ce19acf9ddf5e36f75cf12e033
ahmedaminezaier <zaierahmed200@gmail.com>
2024-07-10 19:29:49 +0100

    Ninja

--- javascript/practice/Ninja/ninja.js
+++ javascript/practice/Ninja/ninja.js
@@ -1,9 +1,9 @@
 class Ninja {
-    constructor(name,health,speed,strength) {
+    constructor(name,health=90,speed=3,strength=3) {
         this.name = name;
-        this.health = health || 90;
-        this.speed = speed || 3;
-        this.strength = strength || 3;
+        this.health = health;
+        this.speed = speed;
+        this.strength = strength;
     }
     
     sayName() {
