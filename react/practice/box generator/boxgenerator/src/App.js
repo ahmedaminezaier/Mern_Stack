05ca541b54628330bcc49596302e450d1c8a3098
ahmedaminezaier <zaierahmed200@gmail.com>
2024-07-28 09:17:33 +0100

    message

---
+++ react/practice/box generator/boxgenerator/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import BoxGenerator from './components/BoxGenerator'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Box Generator</h1>
+      <BoxGenerator />
+    </div>
+  );
+}
+
+export default App;
